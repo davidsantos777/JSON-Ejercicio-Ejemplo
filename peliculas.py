3774abaa6bbfac9cc3e58d5165bddfe636b67eb8
David Santos <35598098+davidsantos777@users.noreply.github.com>
2019-03-25 17:32:29 +0100

    Add files via upload

---
+++ peliculas.py
@@ -0,0 +1,20 @@
+import json
+
+import codecs
+
+doc = json.load(codecs.open('movies.json', 'r', 'utf-8-sig'))
+
+while True:
+	print('''1.- Listar el título, año y duración de todas las películas.
+2.- Mostrar los títulos de las películas y el número de actores/actrices que tiene cada una.
+3.- Mostrar las películas que contengan en la sinopsis dos palabras dadas.
+4.- Mostrar las películas en las que ha trabajado un actor dado.
+5.- Mostrar el título y la url del póster de las tres películas con una media de puntuaciones más alta y lanzadas entre dos fechas dadas.
+0.- Salir''')
+
+	opcion = input("Opción: ")
+
+	if opcion=="1":
+
+	if opcion =="0":
+		break
